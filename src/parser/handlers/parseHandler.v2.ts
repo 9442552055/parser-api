9a3cd409c3e9dc9a85045cf836100f86432acc1d
Arun mozhi Srinivasan <arun.m@msystechnologies.com>
2019-07-29 02:09:16 +0530

    request validation in v2

--- src/parser/handlers/parseHandler.v2.ts
+++ src/parser/handlers/parseHandler.v2.ts
@@ -6,6 +6,18 @@ import FormatedUserStringDeserializer from "../controllers/FormatedUserStringDes
 import FormatedUser from "../schemas/FormatedUser";
 
 function parseHandler(req: Core.Request, callback: (result: Base.IHTTPResult) => void) {
+    // request validation
+    // we can use json schema validation & filters as common middleware
+    // here i am using inline validation
+    if (typeof req.body === "undefined"
+        || Object.keys(req.body).indexOf("data") === -1
+        || typeof req.body.data !== "string") {
+        callback({
+            data: new Results.FailureResult({ message: "Validation failed!" }, "Parsing failed!"),
+            statusCode: 400
+        });
+        return;
+    }
     // planning for dependecy injection here
     // we have control over those handler calls which is triggered from RoutesProvier
     const userStringDeserializer: FormatedUserStringDeserializer =
